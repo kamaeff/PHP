b2d4132cff405f17c7f473c3d725a32c6cb554c8
anton <kamaeff2@gmail.com>
2023-11-29 14:23:08 +0300

    fix style

--- index.php
+++ index.php
@@ -8,12 +8,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./style/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
   <title>Php Lessons</title>
 </head>
 <body>
-<h1 style="text-align: center">Уроки Php</h1>
-  <main class="main">
+  <header>
+    <h1>Уроки Php</h1>
+  </header>
 
+  <main class="main">
     <div class="main_math">
       <h2>Математические действия</h2>
       <form method="post" class="main_math-form">
@@ -26,18 +31,20 @@
           if($status == false){
             echo '<br>Error: Данные должны быть заполнены!!!';
           }else{
-            echo '<div>Ты ввел: a = '. $object->a. ', b = '. $object->b. '</div>
-            <div>Сумма = '. $object->sum(). '</div>
-            <div>Вычитание = '. $object->sub(). '</div>
-            <div>Умножение = '. $object->mult(). '</div>
-            <div>Деление = '. $object->div(). '</div>
-            <div>Остаток от деления = '. $object->getmod(). '</div>';
+            echo '
+              <div>Ты ввел: a = '. $object->a. ', b = '. $object->b. '</div>
+              <div>Сумма = '. $object->sum(). '</div>
+              <div>Вычитание = '. $object->sub(). '</div>
+              <div>Умножение = '. $object->mult(). '</div>
+              <div>Деление = '. $object->div(). '</div>
+              <div>Остаток от деления = '. $object->getmod(). '</div>
+            ';
           }
         ?>
       </div>
     </div>
 
-    
+
   </main>
 </body>
 </html>
